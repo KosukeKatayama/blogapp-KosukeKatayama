da5d0170b4541f41b2d2738699cab11325588e1f
Kosuke Katayama <kosuke.katayama1024@gmail.com>
2021-02-06 23:58:42 +0900

    updateアクション

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -7,6 +7,28 @@ class ProfilesController < ApplicationController
     end
 
     def edit
-        @profile = current_user.build_profile
+        @profile = current_user.prepare_profile
+    end
+
+    def update
+        @profile = current_user.prepare_profile
+        @profile.assign_attributes(profile_params)
+        if @profile.save
+            redirect_to profile_path, notice: 'プロフィール更新！'
+        else
+            flash.now[:error] = '更新できませんでした'
+            render :edit
+        end
+    end
+
+    private
+    def profile_params
+        params.require(:profile).permit(
+            :nickname,
+            :introduction,
+            :gender,
+            :birthday,
+            :subscribed
+        )
     end
 end
