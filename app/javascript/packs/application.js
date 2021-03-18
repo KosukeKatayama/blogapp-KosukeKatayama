d1fb642e2f6f04abcb1e3561d20e0689f5f4fa45
Kosuke Katayama <kosuke.katayama1024@gmail.com>
2021-03-18 16:39:43 +0900

    axiosでgetリクエスト

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,3 +18,15 @@ require("channels")
 
 require("trix")
 require("@rails/actiontext")
+
+import $ from 'jquery'
+import axios from 'axios'
+
+document.addEventListener('turbolinks:load', () => {
+    $('.article_title').on('click', () => {
+        axios.get('/')
+            .then((response) => {
+                console.log(response)
+        })
+    })
+})
