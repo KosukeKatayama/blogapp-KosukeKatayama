75a8c9935526be8730e52de6d9ffb638b1bc8640
Kosuke Katayama <kosuke.katayama1024@gmail.com>
2021-03-18 20:57:12 +0900

    いいねの状態をjQueryで反映

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,6 +21,9 @@ require("@rails/actiontext")
 
 import $ from 'jquery'
 import axios from 'axios'
+import { csrfToken } from 'rails-ujs'
+
+axios.defaults.headers.common ['X-CSRF-Token'] = csrfToken()
 
 const handleHeartDisplay = (hasLiked) => {
     if (hasLiked) {
@@ -38,4 +41,32 @@ document.addEventListener('turbolinks:load', () => {
             const hasLiked = response.data.hasLiked
             handleHeartDisplay(hasLiked)
         })
+
+    $('.inactive-heart').on('click', () => {
+        axios.post(`/articles/${articleId}/like`)
+            .then((response) => {
+                if (response.data.status === 'ok') {
+                    $('.active-heart').removeClass('hidden')
+                    $('.inactive-heart').addClass('hidden')
+                }
+            })
+            .catch((e) => {
+                window.alert('Error')
+                console.log(e)
+            })
+    })
+
+    $('.active-heart').on('click', () => {
+        axios.delete(`/articles/${articleId}/like`)
+            .then((response) => {
+                if (response.data.status === 'ok') {
+                    $('.active-heart').addClass('hidden')
+                    $('.inactive-heart').removeClass('hidden')
+                }
+            })
+            .catch((e) => {
+                window.alert('Error')
+                console.log(e)
+            })
+    })
 })
