d517db400863c84df46d2fee9bb528f5438b9a61
Kosuke Katayama <kosuke.katayama1024@gmail.com>
2021-03-26 18:31:10 +0900

    メールを非同期的に送信するように

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -26,6 +26,6 @@ class Relationship < ApplicationRecord
 
     private
     def send_email
-        RelationshipMailer.new_follower(following, follower).deliver_now
+        RelationshipMailer.new_follower(following, follower).deliver_later
     end
 end
