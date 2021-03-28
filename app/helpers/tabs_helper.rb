39f390d4fb90b27dabe211e34818dc1a8a62c262
Kosuke Katayama <kosuke.katayama1024@gmail.com>
2021-03-28 21:20:03 +0900

    複数言語対応

--- app/helpers/tabs_helper.rb
+++ app/helpers/tabs_helper.rb
@@ -1,6 +1,6 @@
 module TabsHelper
     def add_active_class(path)
-        path = path.split('?').first
+        path = path.split('?').first #remove path after ?
         'active' if current_page?(path)
     end
 end
