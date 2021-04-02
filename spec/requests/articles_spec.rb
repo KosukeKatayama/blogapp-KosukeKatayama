f0036478ec8640e60166667babac7433b4ea7c65
Kosuke Katayama <kosuke.katayama1024@gmail.com>
2021-04-02 18:16:58 +0900

    ログインしていない場合のテスト

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -16,13 +16,21 @@ RSpec.describe 'Articles', type: :request do
       before do
         sign_in user
       end
-      
+
       it '記事が保存される' do
         article_params = attributes_for(:article)
         post articles_path({article: article_params})
         expect(response).to have_http_status(302)
         expect(Article.last.title).to eq(article_params[:title])
         expect(Article.last.content.body.to_plain_text).to eq(article_params[:content])
+      end
+    end
+
+    context 'ログインしていない場合' do
+      it 'ログイン画面に遷移する' do
+        article_params = attributes_for(:article)
+        post articles_path({article: article_params})
+        expect(response).to redirect_to(new_user_session_path)
       end
     end
   end
